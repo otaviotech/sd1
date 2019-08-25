e07b8e0872afbb24c242568c548ec48d12b9a75f
Otavio Araujo <otaviotech@gmail.com>
2019-08-25 18:21:48 -0300

    Adicionando url da aplicacao no swagger.yaml

--- src/utils/env.js
+++ src/utils/env.js
@@ -4,6 +4,7 @@ const packageJson = require('../../package.json');
 module.exports = {
   APP_HOST: process.env.APP_HOST || 'localhost',
   APP_PORT: process.env.PORT,
+  APP_URL: process.env.URL,
   APP_MAJOR_VERSION: semver.major(packageJson.version),
   API_VERSION: semver.major(packageJson.version) + 1,
   SWAGGER_API_KEY: process.env.SWAGGER_API_KEY,
